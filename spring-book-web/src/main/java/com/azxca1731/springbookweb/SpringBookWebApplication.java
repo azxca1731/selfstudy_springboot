c24676284acfadf7cf9119d118edcd83b683614d
azxca1731 <azxca1731@gmail.com>
2019-06-16 16:31:50 +0900

    service 및 controller 설정

--- spring-book-web/src/main/java/com/azxca1731/springbookweb/SpringBookWebApplication.java
+++ spring-book-web/src/main/java/com/azxca1731/springbookweb/SpringBookWebApplication.java
@@ -1,13 +1,52 @@
 package com.azxca1731.springbookweb;
 
+import com.azxca1731.springbookweb.domain.Board;
+import com.azxca1731.springbookweb.domain.User;
+import com.azxca1731.springbookweb.domain.enums.BoardType;
+import com.azxca1731.springbookweb.repository.BoardRepository;
+import com.azxca1731.springbookweb.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SpringBookWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBookWebApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository,
+        BoardRepository boardRepository) throws Exception {
+        return args -> {
+            User user = userRepository.save(
+                    User.builder()
+                            .name("havi")
+                            .password("test")
+                            .email("test@gmail.com")
+                            .createdDate(LocalDateTime.now())
+                            .build()
+            );
+
+            IntStream.rangeClosed(1, 200).forEach(index ->
+                boardRepository.save(
+                        Board.builder()
+                            .title("게시글"+index)
+                            .subTitle("순서"+index)
+                            .content("콘텐츠")
+                            .boardType(BoardType.free)
+                            .createdDate(LocalDateTime.now())
+                            .updatedDate(LocalDateTime.now())
+                            .user(user)
+                            .build()
+                )
+            );
+        };
     }
 
 }
